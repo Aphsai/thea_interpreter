9291ecedc8a8db2faa595ee8d9876de398eb173c
Aphsai <saksham.a@hotmail.ca>
2019-03-11 00:21:18 -0400

    function evaluation now works, added environments

--- object/object.go
+++ object/object.go
@@ -2,6 +2,9 @@ package object
 
 import (
 	"fmt"
+	"thea_interpreter/ast"
+	"bytes"
+	"strings"
 )
 type ObjectType string
 
@@ -16,6 +19,7 @@ const (
 	NULL_OBJ = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type Integer struct {
@@ -53,18 +57,52 @@ func (e *Error) Inspect() string { return "ERROR: " + e.Message }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
+}
+
+func NewEnclosedEnvironment(env *Environment) *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: env}
 }
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n")
+
+	return out.String()
 }
